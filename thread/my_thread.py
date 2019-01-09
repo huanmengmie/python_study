4f4f70e5d593abd23d038e6b4062a9c8d5788c0b
moheyibei <huanmengmie@163.com>
2019-01-09 01:47:40 -0800

    使用tcp协议，多线程模拟聊天室

--- thread/my_thread.py
+++ thread/my_thread.py
@@ -4,24 +4,6 @@ import threading
 from time import ctime
 
 
-def loop():
-    print("threading %s running..." % threading.current_thread().name)
-
-    n = 0
-    while n < 5:
-        print("thread %s => %s" % (threading.current_thread().name, n))
-        n += 1
-    print("threading %s ended..." % threading.current_thread().name)
-
-
-def main():
-    print("threading %s running..." % threading.current_thread().name)
-    t = threading.Thread(target=loop, name="LoopThread")
-    t.start()
-    t.join()
-    print("threading %s ended..." % threading.current_thread().name)
-
-
 class MyThread(threading.Thread):
     def __init__(self, func, args, name=''):
         threading.Thread.__init__(self)
@@ -33,7 +15,3 @@ class MyThread(threading.Thread):
         print('starting', self.name, 'at:', ctime())
         self.func(*self.args)
         print(self.name, 'finished at ', ctime())
-
-
-if __name__ == "__main__":
-    main()
